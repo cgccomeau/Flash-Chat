a72d7cf3f39904e14242f0ef579c66bea6d1f56a
Charlie Comeau <comchara@amazon.com>
2021-12-17 17:42:03 -0800

    initial commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Comeau, Charlie on 12/7/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
